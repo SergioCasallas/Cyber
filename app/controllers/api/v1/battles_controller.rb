365fe8f463144fbe246cd659cd833b3ad826f567
Miguel Angel Meneses Alvarez <mamalvarez11@gmail.com>
2021-06-27 14:30:39 -0500

    Rename key

--- app/controllers/api/v1/battles_controller.rb
+++ app/controllers/api/v1/battles_controller.rb
@@ -1,15 +1,12 @@
 class Api::V1::BattlesController < ApplicationController
 
   def create
-    puts "=" * 200
-    puts "#{payload_params}"
-    puts "=" * 200
-    BattleService.(payload_params)
+    BattleService.(data_params)
   end
 
   private
 
-  def payload_params
-    params.permit(:payload)
+  def data_params
+    params.permit(:data).deep_transform_keys { |key| key.to_sym }
   end
 end
